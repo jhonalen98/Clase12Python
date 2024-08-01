ea4219da73be82585073aaf6c9a37ffd4584a674
jhonalen98 <jhon.gonzalez@tecnologicoliceocristiano.edu.ec>
2024-08-01 14:46:12 -0500

    new file:   Imprimir.py new file:   NuevaSecuencia.py

---
+++ Imprimir.py
@@ -0,0 +1,3 @@
+Palabra=(input("Ingrese una palabra: "))
+ListaFinal=Palabra[1:]+Palabra[0]
+print(ListaFinal)
---
+++ NuevaSecuencia.py
@@ -0,0 +1,7 @@
+Numero=int(input("Ingrese un numero: "))
+Limite=range(1,numero+1)
+NumeroEntero=""
+
+for indice in NumeroEntero:
+    NumeroEntero+=str(indice)
+    print(NumeroEntero)
